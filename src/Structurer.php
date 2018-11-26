ae051b0d8c32833ed17c3d91c1f0dc9c88f1fe71
adikalon <webadev@yandex.ru>
2018-11-26 15:37:59 +0300

    Версия 2.0.0

--- src/Structurer.php
+++ src/Structurer.php
@@ -2,11 +2,11 @@
 
 namespace Hellpers;
 
-use \Exception;
+use Exception;
 
 /**
- * Создание/воссоздание структуры пути
- *
+ * Работа с файловой системой
+ * 
  * @author adikalon
  */
 class Structurer
@@ -14,121 +14,303 @@ class Structurer
     /**
      * @var string Абсолютный путь к корню приложения
      */
-    private $core = '';
+    private $core;
+
+    /**
+     * @var string Имя последнего файла с которым велась работа
+     */
+    private $file;
+
+    /**
+     * @var array Шаблон-обертка для символов, которые необходимо прогнать
+     * через date() 
+     */
+    private static $template = [
+        'before' => '::d->(::',
+        'after'  => '::);::',
+    ];
 
     /**
      * Инициализация
-     *
+     * 
      * @param string $core Абсолютный путь к корню приложения
      * @throws Exception
      */
-    public function __construct($core = '')
+    public function __construct(string $core)
     {
-        $this->core = $core;
+        $this->core = realpath($core);
+
         unset($core);
 
-        // Валидация корня приложения
-        if (!is_string($this->core)) {
-            throw new Exception(Dencoder::utf8('Корень приложения (параметр "core") должен быть строкой.'));
-        } else {
-            if (!realpath($this->core) or !is_dir(realpath($this->core))) {
-                throw new Exception(Dencoder::utf8('Некорректный адрес приложения: "' . $this->core . '".'));
-            } else {
-                $this->core = realpath($this->core);
-            }
+        if (!$this->core or !is_dir($this->core)) {
+            throw new Exception("Некорректный адрес приложения: {$this->core}");
         }
 
-        if ($this->core != '/') {
-            $this->core = preg_replace('/\/+$/ui', '', $this->core);
-        }
+        $this->core = Pather::upath($this->core);
+        $this->core = Pather::rstrim($this->core);
     }
 
     /**
+     * Создание папки и/или переход в нее
      * 
-     * @return self::q Статический экронирующий метод
+     * @param string $name Имя для папки
+     * @param int $mode (optional) Права доступа (в восьмеричной системе
+     * счисления)
+     * @return self Новый объект с новым корневым путем
      */
-    public function __call($name, $arguments)
+    public function folder(string $name, int $mode = 0777): self
     {
-        if ($name == 'q') {
-            return self::q($arguments[0]);
-        }
+        $name = Pather::name($name);
+        $path = "{$this->core}/$name";
+        $make = self::make($path, '', $mode);
+
+        unset($name, $mode, $path);
+
+        return new self($make);
     }
 
     /**
-     * Получение, проверка и создание пути
-     * @param string $path Внутренний путь к дирректории сохранения логов.
-     * В качестве разделителя директорий используется - "/".
-     * "\" - Используется для экранирования, т.к. присутствует поддержка спецсимволов PHP-функции date().
-     * Обратные ссылки ".." - будут проигнорированы.
-     * @param string $file Имя файла (при необходимости создать файл). Присутствует поддержка спецсимволов PHP-функции date().
-     * @return string Абсолютный путь
+     * Подняться на директорией выше. Не сработает в корне
+     * 
+     * @return self Новый объект, с новым корневым путем
+     */
+    public function back(): self
+    {
+        $path = preg_replace('/\/[^\/]+$/ui', '', $this->core);
+
+        return new self($path);
+    }
+
+    /**
+     * Создание файла и/или установка внутреннего курсора на него
+     * 
+     * @param string $name Имя для файла
+     * @param int $mode (optional) Права доступа (в восьмеричной системе
+     * счисления)
+     * @return self Текущий объект с курсором установленным на файле
+     */
+    public function file(string $name, int $mode = 0777): self
+    {
+        $name       = Pather::name($name);
+        $path       = self::make($this->core, $name, $mode);
+        $this->file = preg_replace('/.*\//ui', '', $path);
+
+        unset($name, $mode, $path);
+
+        return $this;
+    }
+
+    /**
+     * Записать текст в файл на котором установлен курсор
+     * 
+     * @param string $text Текст, который необходимо записать
+     * @param bool $append (optional) true - дописать в файл
+     * @return self Текущий объект
      * @throws Exception
      */
-    public function get($path = '', $file = '')
+    public function content(string $text, bool $append = false): self
     {
-        // Валидация внутреннего пути
-        if (!is_string($path)) {
-            throw new Exception(Dencoder::utf8('Внутренний путь (параметр "$path") должен быть строкой.'));
+        if (!$this->file) {
+            throw new Exception(
+                "Отсутствует файл для записи. Директория: {$this->core}"
+            );
         }
 
-        // Валидация имени файла
-        if (!is_string($file)) {
-            throw new Exception(Dencoder::utf8('Имя файла (параметр "$file") должно быть строкой.'));
+        if ($append) {
+            $append = FILE_APPEND;
         }
 
-        // Обрезаем слеши в начале и конце пути
-        $path = preg_replace(['/^\/+/ui', '/\/+$/ui'], '', $path);
+        $path = "{$this->core}/{$this->file}";
 
-        // Создание структуры директорий
-        if (!file_exists($this->core . '/' . $path)) {
-            $structure = explode('/', $path);
-            $path      = '';
-            foreach ($structure as $folder) {
-                $folder = Dencoder::name(date($folder));
-                if (empty($folder) or $folder == '..' or $folder == '.') {
-                    unset($folder);
-                    continue;
-                }
-                $path .= '/' . $folder;
-                $path = preg_replace(['/^\/+/ui', '/\/+$/ui'], '', $path);
-                if (!file_exists($this->core . '/' . $path)) {
-                    if (!mkdir($this->core . '/' . $path)) {
-                        throw new Exception(Dencoder::utf8('Не удалось создать папку: "' . $this->core . '/' . $path . '".'));
-                    }
-                }
-                unset($folder);
-            }
+        if (file_put_contents($path, $text, $append) === false) {
+            throw new Exception("Не удалось записать в файл: $path");
         }
-        unset($structure);
-        $path = $this->core . '/' . $path;
 
-        // Создание файла
-        if (!empty(trim($file))) {
-            $path = $path . '/' . Dencoder::name(date($file));
-            if (!file_exists($path)) {
-                $fp = fopen($path, 'ab');
-                if (!$fp) {
-                    throw new Exception(Dencoder::utf8('Не удалось создать файл: "' . $path . '".'));
-                }
-                fclose($fp);
-                unset($fp);
-            }
-        }
-        unset($file);
-        return $path;
+        unset($text, $append, $path);
+
+        return $this;
     }
 
     /**
-     * Экраинирование каждой буквы
-     * @param string $string Строка
-     * @return string Строка, где каждая буква экранирована
+     * Смена прав доступа, владельца, группы для файла/папки
+     * 
+     * @param array $params Что менять
+     * 
+     * int $params['chmod'] (optional) - Права доступа (в восьмеричной системе
+     * счисления)
+     * 
+     * mixed $params['chown'] (optional) - Владелец (имя или число)
+     * 
+     * mixed $params['chgrp'] (optional) - Группа (название или номер)
+     * 
+     * @param bool $folder (optional) true - применить для текущей директории
+     * @return self Текущий объект
+     * @throws Exception
      */
-    public static function q($string)
+    public function mode(array $params, bool $folder = false): self
     {
-        if (!is_string($string)) {
-            throw new Exception(Dencoder::utf8('Параметр "$string" должен быть строкой.'));
+        $path = $this->core;
+
+        if ($this->file and !$folder) {
+            $path .= "/{$this->file}";
         }
-        return Dencoder::quote($string);
+
+        if (isset($params['chmod'])) {
+            if (!chmod($path, $params['chmod'])) {
+                throw new Exception("Не удалось изменить режим доступа: $path");
+            }
+        }
+
+        if (isset($params['chown'])) {
+            if (!chown($path, $params['chown'])) {
+                throw new Exception("Не удалось изменить владельца: $path");
+            }
+        }
+
+        if (isset($params['chgrp'])) {
+            if (!chgrp($path, $params['chgrp'])) {
+                throw new Exception("Не удалось изменить группу: $path");
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Получить абсолютный путь к текущей директории
+     * 
+     * @param bool $file (optional) true - вернуть вместе с файлом, на котором
+     * остался курсор
+     * @return string Абсолютный путь
+     */
+    public function path(bool $file = false): string
+    {
+        if ($file and $this->file) {
+            unset($file);
+            return "{$this->core}/{$this->file}";
+        } else {
+            unset($file);
+            return $this->core;
+        }
+    }
+
+    /**
+     * Получить список файлов внутри директории
+     * 
+     * @return array Список файлов
+     */
+    public function ls(): array
+    {
+        $content = [];
+        $scan = scandir($this->core);
+
+        while (($i = array_search('.', $scan)) !== false) {
+            unset($scan[$i]);
+        }
+
+        while (($i = array_search('..', $scan)) !== false) {
+            unset($scan[$i]);
+        }
+
+        if ($scan) {
+            $content = array_values($scan);
+        }
+
+        unset($scan, $i);
+
+        return $content;
+    }
+
+    /**
+     * Создать шаблон для преобразования функцией date()
+     * 
+     * @param string $string Строка содержащая спецсиволы
+     * @return string Строка обернутая шаблоном для декодирования
+     */
+    public static function d(string $string): string
+    {
+        return self::$template['before'] . $string . self::$template['after'];
+    }
+
+    /**
+     * Создание структуры директорий и/или файла
+     * 
+     * @param string $path Абсолютный путь, который необходимо реализовать
+     * @param string $file (optional) Имя для файла
+     * @param int $mode (optional) Права доступа (в восьмеричной системе
+     * счисления)
+     * @return string Абсолютный путь к финальной папке/файлу
+     * @throws Exception
+     */
+    public static function make(
+        string $path,
+        string $file = '',
+        int $mode = 0777
+    ): string
+    {
+        $pattern = '/'
+            . preg_quote(self::$template['before'])
+            . '(.*)'
+            . preg_quote(self::$template['after'])
+            . '/ui';
+        
+        $npath = Pather::expath($path, ['rstrim' => true]);
+        $npath = preg_replace_callback(
+            $pattern, [__CLASS__, '_dateCB'], $npath
+        );
+
+        if (!file_exists($npath)) {
+            if (!mkdir($npath, $mode, true)) {
+                throw new Exception("Не удалось создать структуру пути: $path");
+            }
+        }
+
+        if ($file) {
+            $file  = preg_replace_callback(
+                $pattern, [__CLASS__, '_dateCB'], $file
+            );
+            $npath = "$npath/$file";
+            if (!file_exists($npath)) {
+                if (file_put_contents($npath, '') === false) {
+                    throw new Exception("Не удалось создать файл: $npath");
+                }
+            }
+            if (!chmod($npath, $mode)) {
+                throw new Exception(
+                    "Не удалось установить права "
+                    . sprintf('%04o', $mode)
+                    . " файлу: $npath"
+                );
+            }
+        }
+
+        unset($path, $file, $pattern);
+
+        return $npath;
+    }
+
+    /**
+     * Callback декодирования шаблона функцией preg_replace_callback()
+     * 
+     * @param array $match То, что приходит из preg_replace_callback()
+     * @return string Преобразованная строка
+     */
+    private static function _dateCB(array $match): string
+    {
+        $string = $match[0];
+        $string = str_replace(
+            [
+                self::$template['before'],
+                self::$template['after']
+            ],
+            '',
+            $string
+        );
+        $string = date($string);
+
+        unset($match);
+
+        return $string;
     }
 
 }
